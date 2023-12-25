a89842d728fffa7d6ac321b1a5f3d60e684121ab
Anas Siddiqui <connect.anas.dev@gmail.com>
2023-12-26 00:04:57 +0530

    Modified jenkinsfile

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ cd /home/ec2-user/
 git clone https://github.com/Anas-Ahmad-Siddiqui/jenkins-ec2-first_flight
 
 cd /home/ec2-user/jenkins-ec2-first_flight
-/bin/bash /scripts/before_install.sh
-/bin/bash /scripts/after_install.sh
-/bin/bash /scripts/befapplication_start.sh
+/bin/bash /home/ec2-user/jenkins-ec2-first_flight/scripts/before_install.sh
+/bin/bash /home/ec2-user/jenkins-ec2-first_flight/scripts/after_install.sh
+/bin/bash /home/ec2-user/jenkins-ec2-first_flight/scripts/befapplication_start.sh
