3c8f40943c37928de62d75cc809e9c9758ef1395
Anas Siddiqui <connect.anas.dev@gmail.com>
2023-12-26 04:56:23 +0530

    Modified Jenkinsfile

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,10 +4,10 @@ echo 'run application_start.sh: ' >> /home/ec2-user/jenkins-ec2-first_flight/dep
 
 cd /home/ec2-user/jenkins-ec2-first_flight >> /home/ec2-user/jenkins-ec2-first_flight/deploy.log
 pm2 delete all
-pm2 start server.js --name=jenkins-app
-pm2 save
+# pm2 start server.js --name=jenkins-app
+# pm2 save
 # pm2 startup
 
 # nodejs-app is the same name as stored in pm2 process
-echo 'pm2 restart expressjs-app' >> /home/ec2-user/jenkins-ec2-first_flight/deploy.log
-pm2 restart expressjs-app >> /home/ec2-user/jenkins-ec2-first_flight/deploy.log
+# echo 'pm2 restart expressjs-app' >> /home/ec2-user/jenkins-ec2-first_flight/deploy.log
+# pm2 restart expressjs-app >> /home/ec2-user/jenkins-ec2-first_flight/deploy.log
