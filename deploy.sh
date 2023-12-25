7d1d80c01fe1cbd7beb52f5512f0088b3563cd7b
Anas Siddiqui <connect.anas.dev@gmail.com>
2023-12-26 04:55:21 +0530

    Modified Jenkinsfile

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,6 @@ cd /home/ec2-user/jenkins-ec2-first_flight
 
 /bin/bash /home/ec2-user/jenkins-ec2-first_flight/scripts/before_install.sh
 
-# /bin/bash /home/ec2-user/jenkins-ec2-first_flight/scripts/after_install.sh
+/bin/bash /home/ec2-user/jenkins-ec2-first_flight/scripts/after_install.sh
 
 # /bin/bash /home/ec2-user/jenkins-ec2-first_flight/scripts/application_start.sh
