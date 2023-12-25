d43f6f86f4537886167bea99b49b02825cb00815
Anas Siddiqui <connect.anas.dev@gmail.com>
2023-12-26 04:53:31 +0530

    Modified Jenkinsfile

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,11 +4,11 @@ sudo yum update -y
 sudo yum upgrade -y
 sudo yum install -y git
 
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-# . ~/.nvm/nvm.sh
-# nvm install --lts
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
 
-# npm install pm2@latest -g
+npm install pm2@latest -g
 
 # sudo ln -sf "$(which node)" /sbin/node
 # sudo ln -sf "$(which npm)" /sbin/npm
