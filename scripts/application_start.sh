84000dd3d9cc0179bb474ef084e38bd2fb226576
Anas Siddiqui <connect.anas.dev@gmail.com>
2024-01-01 23:28:25 +0530

    Modified /scripts

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,11 +4,11 @@ echo 'run application_start.sh: ' >> /home/ec2-user/jenkins-ec2-first_flight/dep
 
 cd /home/ec2-user/jenkins-ec2-first_flight >> /home/ec2-user/jenkins-ec2-first_flight/deploy.log
 
-pm2 delete all >> /home/ec2-user/jenkins-ec2-first_flight/deploy.log
-pm2 start server.js --name=jenkins-app >> /home/ec2-user/jenkins-ec2-first_flight/deploy.log
+sudo pm2 delete jenkins-app >> /home/ec2-user/jenkins-ec2-first_flight/deploy.log
+sudo pm2 start server.js --name=jenkins-app >> /home/ec2-user/jenkins-ec2-first_flight/deploy.log
 
-pm2 save >> /home/ec2-user/jenkins-ec2-first_flight/deploy.log
+suod pm2 save >> /home/ec2-user/jenkins-ec2-first_flight/deploy.log
 sudo pm2 startup systemd >> /home/ec2-user/jenkins-ec2-first_flight/deploy.log
 
 echo 'pm2 restart jenkins-app' >> /home/ec2-user/jenkins-ec2-first_flight/deploy.log
-pm2 restart jenkins-app >> /home/ec2-user/jenkins-ec2-first_flight/deploy.log
+sudo pm2 restart jenkins-app >> /home/ec2-user/jenkins-ec2-first_flight/deploy.log
